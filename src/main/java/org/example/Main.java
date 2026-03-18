da6a2f5ab6cc0ad75dc82a81209bce8ed1a3022b
suryajegathesan <surya@bizappln.com>
2026-03-18 10:22:29 +0530

    Added New Line from Dell

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,5 +7,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Test");
+
+        System.out.println("Test 2 from machine 2");
     }
 }
